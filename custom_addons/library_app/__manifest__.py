196d64a11865f21b4814fa06b2d4bfe9ed4f09b5
philipotieno <iamphilipotieno@gmail.com>
2022-04-04 09:33:03 +0300

    chore: add manifest file

--- custom_addons/library_app/__manifest__.py
+++ custom_addons/library_app/__manifest__.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "library_app",
+    'name': "Library Management",
 
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    'summary': "Manage library catalogue and book lending",
 
     'description': """
         Long description of module's purpose
     """,
 
-    'author': "My Company",
+    'author': "Philip Otieno",
+    'license': "AGPL -3",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    "sequence": -103,
+    'category': 'Productivity',
+    'version': "15.0.1.0.0",
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
@@ -28,8 +28,6 @@
         'views/views.xml',
         'views/templates.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    "installable": True,
+    "application": True,
 }
