1b08da53c60cfb8b35cd2a73971c12098655e4e0
philipotieno <iamphilipotieno@gmail.com>
2022-04-05 07:51:16 +0300

    chore: add a library icon

--- custom_addons/library_app/__manifest__.py
+++ custom_addons/library_app/__manifest__.py
@@ -9,14 +9,14 @@
     """,
 
     'author': "Philip Otieno",
-    'license': "AGPL -3",
+    'license': "AGPL-3",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     "sequence": -103,
-    'category': 'Productivity',
+    'category': 'Services/Library',
     'version': "15.0.1.0.0",
 
     # any module necessary for this one to work correctly
