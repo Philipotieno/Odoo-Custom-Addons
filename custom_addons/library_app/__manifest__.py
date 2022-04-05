13087cf36af0dc4b766be52a22750f65a582826b
philipotieno <iamphilipotieno@gmail.com>
2022-04-05 21:43:55 +0300

    chore: add user and manager security

--- custom_addons/library_app/__manifest__.py
+++ custom_addons/library_app/__manifest__.py
@@ -25,7 +25,8 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
+        'security/library_security.xml',
+        'views/library_menu.xml',
         'views/templates.xml',
     ],
     "installable": True,
